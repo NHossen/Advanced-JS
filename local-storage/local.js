665161af258d54c25d1a68a64bf718d943af03f1
Naeem Hossen <132334814+NHossen@users.noreply.github.com>
2023-09-03 01:49:39 +0400

    learn about how loacl storage working

---
+++ local-storage/local.js
@@ -0,0 +1,7 @@
+const hello=localStorage.getItem("This is naeem");
+console.log(hello);
+
+const setAge=()=>{
+    localStorage.setItem('age',52);
+    localStorage.setItem('name',"naeem Hossen");
+}
